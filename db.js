fb6c602e0761b216dbfe8fd1467229f0c8d52c0d
KimYuRa2 <ururur68@gmail.com>
2022-06-23 16:54:58 +0900

    0623111

--- db.js
+++ db.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 /* 리스트 전체를 불러오는 함수 */
 
 function getAllNotice(callback){
-    connection.query('select * from notice ORDER BY id DESC',
+    connection.query('select * from heynature ORDER BY id DESC',
     (err, rows, fields) => {
         for(let i=0; i<rows.length;i++){
             /* YYYY-MM-DD 형식으로 출력할 것 */
@@ -31,7 +31,7 @@ function getAllNotice(callback){
 /* 리스트에 새로운 내용을 추가하는 함수 */
 
 function insertNotice(title,content,callback){
-    connection.query(`insert into notice (title,create_time, update_time,content) values ("${title}",now(),now(),"${content}")`,(err,result)=>{
+    connection.query(`insert into heynature (title,create_time, update_time,content) values ("${title}",now(),now(),"${content}")`,(err,result)=>{
         if(err) throw err;
         callback();
     })
@@ -42,7 +42,7 @@ function insertNotice(title,content,callback){
 
 /* 리스트 중 id값이 일치하는 row만 불러오는 함수 */
 function getNoticeById(id, callback){
-    connection.query(`select * from notice where id=${id}`,
+    connection.query(`select * from heynature where id=${id}`,
     (err, row, fields) => {
         if(err) throw err;
         callback(row);
@@ -53,7 +53,7 @@ function getNoticeById(id, callback){
 /* 리스트를 수정하고 싶을 때 id값이 일치하는 부분을 수정하는 함수 */
 
 function updateNoticeById(id, title, content, callback){
-    connection.query(`update notice set title='${title}',update_time=now(), content='${content}' where id=${id}`, (err, result)=>{
+    connection.query(`update heynature set title='${title}',update_time=now(), content='${content}' where id=${id}`, (err, result)=>{
         if(err) throw err;
         callback();
     })
@@ -62,7 +62,7 @@ function updateNoticeById(id, title, content, callback){
 /* 리스트 중 id값이 일치하는 부분을 삭제하는 함수 */
 
 function deleteNoticeById(id,callback){
-    connection.query(`delete from notice where id=${id}`,
+    connection.query(`delete from heynature where id=${id}`,
     (err, result) => {
         if(err) throw err;
         callback();
